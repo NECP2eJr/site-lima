dac75fc0b88f9f48053e16f60a60366bfe9c197e
Vinícius Santos <viniciusssantos.pa@gmail.com>
2024-11-26 22:04:02 -0300

    feat(about): created about component with responsivity

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -2,12 +2,14 @@ import Header from "../components/Header";
 import Footer from "../components/Footer";
 import React from "react";
 import Hero from "@/components/Hero";
+import About from "@/components/About";
 
 const MainPage = () => {
     return (
         <React.Fragment>
             <Header />
             <Hero />
+            <About />
             <Footer />
         </React.Fragment>
     )
